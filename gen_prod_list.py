6a7d6a99e29d1c9ca01c25bece2c375ad74efee3
Sai Chandramouli <schand31@asu.edu>
2016-05-26 21:47:36 -0700

    sai is cleverer than stupid website

--- gen_prod_list.py
+++ gen_prod_list.py
@@ -25,13 +25,15 @@ if __name__ == '__main__':
         'Accept-Language':' en-US,en;q=0.5',
         'Accept-Encoding':' gzip, deflate, br',
         'Referer': 'https://www.ribbon-gifts.com/11906OPNCONSAmway/shop/all-occasions--100-ribbon--treasure/_/N-117929',
-        'Cookie':' BIGipServerpool_p_www.ribbon-gifts.com_all=84781248.0.0000; JSESSIONID=B4310C8F76B0BCF8E1F9DF59C580908A; ccu=C2OzISe+nbcowSOkIxgczWBumiUo/lpK; s_fid=3F6BC5BE5EA9C971-22F26D2960C395AA; s_ppn=ahq%3Aen%3Atreasure%3Alisting; s_ppvl=ahq%253Aen%253Atreasure%253Alisting%2C49%2C49%2C734%2C1173%2C734%2C1440%2C900%2C2%2CP; s_ppv=ahq%253Aen%253Atreasure%253Alisting%2C100%2C48%2C1522%2C1173%2C433%2C1440%2C900%2C2%2CP; s_getNewRepeat=1464320558461-New; s_vnum=1464764400833%26vn%3D1; s_invisit=true; s_dslv=1464320558461; s_dslv_s=First%20Visit; s_cc=true; s_sq=%5B%5BB%5D%5D; BVImplMain%20Site=16188',
+        'Cookie':"BIGipServerpool_p_www.ribbon-gifts.com_all=84781248.0.0000; JSESSIONID=F5D9326F436D35F4EE1A792D6A62E248; ccu=C2OzISe+nbcowSOkIxgczWBumiUo/lpK; s_fid=3F6BC5BE5EA9C971-22F26D2960C395AA; s_ppvl=ahq%253Aen%253Aall%2520occasions%253Alisting%2C65%2C47%2C1039%2C1173%2C433%2C1440%2C900%2C2%2CP; s_ppv=ahq%253Aen%253Aall%2520occasions%253Alisting%2C100%2C65%2C1598%2C1173%2C433%2C1440%2C900%2C2%2CP; s_getNewRepeat=1464324095204-Repeat; s_vnum=1464764400833%26vn%3D2; s_dslv=1464324095204; s_cc=true; s_sq=maritzprod%3D%2526pid%253Dahq%25253Aen%25253Aall%252520occasions%25253Alisting%2526pidt%253D1%2526oid%253Dhttps%25253A%25252F%25252Fwww.ribbon-gifts.com%25252F11906OPNCONSAmway%25252Fbrowse%25252Fcategory.jsp%25253FcatId%25253Dcat290019%2526ot%253DA; BVImplMain%20Site=16188; s_ppn=ahq%3Aen%3Aall%20occasions%3Alisting; s_invisit=true; s_dslv_s=Less%20than%201%20day",
         'Connection':' keep-alive',
         'Cache-Control':' max-age=0'
     }
 
-    url = 'https://www.ribbon-gifts.com/11906OPNCONSAmway/shop/treasure/_/N-118025'
+    url = 'https://www.ribbon-gifts.com/11906OPNCONSAmway/shop/treasure/_/N-118025?No=0&Nrpp=48'
 
     text = get_prods_html(url, 'get', headers)
     html = soupify(text)
     print(html)
+    with open('prod.html', 'w') as ht:
+        ht.write(html.encode('ascii', 'ignore'))
