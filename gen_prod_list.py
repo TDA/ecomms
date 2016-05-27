751aad55349023fd266d29969aa432b73a8737d8
Sai Chandramouli <schand31@asu.edu>
2016-05-26 21:07:45 -0700

    wrote a soupify with no exception throwing

--- gen_prod_list.py
+++ gen_prod_list.py
@@ -1,9 +1,17 @@
 __author__ = 'saipc'
 
 import requests
+from bs4 import BeautifulSoup
 
 def get_prods(url, method, headers=None):
     if str(method).lower() == 'get':
         r = requests.get(url, None, headers=headers)
     else:
         r = requests.post(url, None,headers=headers)
+
+def soupify(text):
+    try:
+        s = BeautifulSoup(text)
+    except Exception as e:
+        return None
+    return s
